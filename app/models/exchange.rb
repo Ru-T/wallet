84cfbae65e629890dcf4c287649aa34d52f3505f
Ru-T <rwajnberg@gmail.com>
2015-09-30 16:18:50 -0400

    Add sum of all exchanges.

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -2,12 +2,25 @@ class Exchange < ActiveRecord::Base
   validates :recipient, presence: true
   validates :amount, presence: true
 
-  def sum_exchanges
-    amount.sum
+  def self.sum_exchanges
+    # sum = 0
+    # Exchange.all.each do |e|
+    #   sum += e.amount
+    # end
+    # sum
+    # #
+    # #
+    self.all.reduce(0) {|sum, exchange| sum + exchange.amount}
   end
 
-  def number_transactions
+  def self.number_exchanges
     self.count
   end
+
+  # def self.number_exchanges_this_month
+  #   self.where("created_at" > "2014")
+  # end
+
+
 
 end
--- test/models/exchange_test.rb
+++ test/models/exchange_test.rb
@@ -1,16 +1,25 @@
 require 'test_helper'
 
 class ExchangeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def test_create_new_exchange
+    salary = Exchange.new(recipient: "salary", amount: 5000)
+    assert salary.save
+    assert_raises(ArgumentError) do
+      Exchange.create!(1, 2, 3)
+    end
+  end
 
   def test_sum_exchanges
-
+    assert_equal 1900.72, Exchange.sum_exchanges
   end
 
   def test_number_transactions
-
+    assert_equal 2, Exchange.number_exchanges
   end
+
+  # def test_transactions_this_month
+  #   assert_equal 1, Exchange.number_exchanges_this_month
+  # end
 
 end
