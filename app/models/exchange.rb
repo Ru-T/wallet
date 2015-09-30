33c1d083215ac78da3934a2acac0e1ef9f0f1537
Ru-T <rwajnberg@gmail.com>
2015-09-30 13:23:50 -0400

    Add metrics on to the dashboard root view.

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,2 +1,13 @@
 class Exchange < ActiveRecord::Base
+  validates :recipient, presence: true
+  validates :amount, presence: true
+
+  def sum_exchanges
+    amount.sum
+  end
+
+  def number_transactions
+    self.count
+  end
+
 end
--- test/models/exchange_test.rb
+++ test/models/exchange_test.rb
@@ -4,4 +4,13 @@ class ExchangeTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def test_sum_exchanges
+
+  end
+
+  def test_number_transactions
+
+  end
+
 end
